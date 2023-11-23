3e1d2e84f9a6d43a857c1ec518499188194cb535
Abbas Abdallah <abbasab13@outlook.com>
2023-11-23 18:19:50 +0200

    first update

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const bodyParser = require('body-parser');
+const cors = require('cors'); // Import the cors middleware
+
+
+// Create an Express app
+const app = express();
+app.use(cors())
+// Create an HTTP server using the Express app
+const server = http.createServer(app);
+
+// Create a Socket.IO instance attached to the HTTP server
+const io = socketIo(server, {
+    cors: {
+      origin: '*',
+    }
+});
+
+// Use body-parser middleware to parse JSON requests
+app.use(bodyParser.json());
+
+
+// Handle Swish webhook endpoint
+app.post('/api/success', (req, res) => {
+  const paymentDetails = req.body;
+
+  // Assuming you've stored the paymentDetails in a variable or database
+  // Emit the paymentDetails to all connected clients
+  console.log(paymentDetails)
+  io.emit('paymentDetails', paymentDetails);
+
+  // Respond to Swish with a success message
+  res.status(200).json({ message: 'Payment details received successfully' });
+});
+
+// Start the server on a specific port
+const port = process.env.PORT || 3001;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
